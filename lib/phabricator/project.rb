ad3d5eb1c5a1c5ea05c159b2a0c825bb85a8cfa0
Jay Shirley <jshirley@stripe.com>
2015-10-27 12:51:15 -0700

    Downcase the cache keys

--- lib/phabricator/project.rb
+++ lib/phabricator/project.rb
@@ -9,14 +9,14 @@ module Phabricator
 
     def self.populate_all
       query.each do |project|
-        @@cached_projects[project.name] = project
+        @@cached_projects[project.name.downcase] = project
       end
     end
 
     def self.find_by_name(name)
       populate_all if @@cached_projects.empty?
 
-      @@cached_projects[name] || refresh_cache_for_project(name)
+      @@cached_projects[name.downcase] || refresh_cache_for_project(name)
     end
 
     def self.raw_value_from_name(name)
@@ -32,10 +32,10 @@ module Phabricator
 
     def self.refresh_cache_for_project(name)
       query(names: [name]).each do |project|
-        @@cached_projects[project.name] = project
+        @@cached_projects[project.name.downcase] = project
       end
 
-      @@cached_projects[name]
+      @@cached_projects[name.downcase]
     end
   end
 end
